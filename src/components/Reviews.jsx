8410776ba65af8d1bdac2bdb702c047b351e506e
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-10-01 11:29:20 +0300

    cast & reviews chapters

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,11 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { getMovieReviewsById, POSTER_CONFIG} from "services/Api";
-
-// const POSTER_CONFIG = {
-//     baseUrl: "http://image.tmdb.org/t/p/",
-//     posterSizes: ["w92", "w154", "w185", "w342", "w500", "w780", "original"],
-// };
+import { getMovieReviewsById} from "services/Api";
 
 const Reviews = () => {
     const [reviews, setReviews] = useState(null);
